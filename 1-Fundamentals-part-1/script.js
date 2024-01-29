a087c1628303a77100f8b6eedc6ea8b84d53795e
unknown <imtiazshakil203@gmail.com>
2024-01-29 17:40:52 +0600

    values and variables

---
+++ 1-Fundamentals-part-1/script.js
@@ -0,0 +1,27 @@
+// values and variables
+
+
+let js="amazing";
+console.log(js);
+
+console.log(40 + 8 + 23 - 10);
+console.log("jonas");
+console.log(23);
+
+let firstName = "Matilada";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilada = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI =3.1416;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+let job1 = "Programmer";
+let job2 = "Teacher";
