ba5619abea8cdf3e361b19934939ec4014bc285e
unknown <imtiazshakil203@gmail.com>
2024-01-30 17:14:43 +0600

    Operators Precedence

--- 1-Fundamentals-part-1/script.js
+++ 1-Fundamentals-part-1/script.js
@@ -1,3 +1,83 @@
+/*
+// values and variables
+
+
+let js="amazing";
+console.log(js);
+
+console.log(40 + 8 + 23 - 10);
+console.log("jonas");
+console.log(23);
+
+let firstName = "Matilada";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// variable name conventions
+let jonas_matilada = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI =3.1416;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+let job1 = "Programmer";
+let job2 = "Teacher";
+
+console.log(myFirstjob);
+
+
+               // Data Type//
+
+// boolean
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+
+// number and string
+console.log(typeof 23);
+console.log(typeof 'jishan');
+
+// boolean to string
+javascriptIsFun = 'YES!'; // don't use 'let' when we use same variable for the next times.
+console.log(typeof javascriptIsFun);
+
+// undefined
+let year;
+console.log(year);
+console.log(typeof year);
+
+//number
+year= 1991;
+console.log(typeof year);
+
+// null
+console.log(typeof null);
+
+
+       // let , const , var //
+
+
+let age = 30;
+age = 31;
+console.log(age);
+
+const birthYear = 1991;
+//birthYear = 1990;  it doesn't work because of without const thae variables doesn't work.
+//const job;   without values const doesn't work.
+
+var job= 'Programmer';
+job = 'teacher';
+console.log(job); // we never use var although 'var' ande 'let' are same but not always .var is old.
+
+lastName = 'pasha';
+console.log(lastName); // but we never write like this ;
 
 
     //   Basic Operators//
@@ -32,3 +112,17 @@ const isFullAge = ageSarah <= 18;
 console.log(isFullAge);
 
 console.log(now - 1991 > now - 2018);
+*/
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+const averageAge = ageJonas + ageSarah / 2;
+console.log(ageJonas, ageSarah, averageAge); 
