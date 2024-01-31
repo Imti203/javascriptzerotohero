1dd012e4ae45d9b6ba6031591a177b5987289353
unknown <imtiazshakil203@gmail.com>
2024-01-31 11:25:50 +0600

    coding challenge #2

--- 1-Fundamentals-part-1/script.js
+++ 1-Fundamentals-part-1/script.js
@@ -194,7 +194,7 @@ console.log('String with \n\ multiple \n\ lines');
 console.log(`String with
  multiple
   lines`);
-  */
+  
 
           // Taking decisions if else statements//
           
@@ -214,3 +214,26 @@ if (birthYear <= 2000) {
   century = 21;
 }
 console.log(century);
+*/
+
+      // coding challenge #2//
+
+const massMark = 78;
+const massJohn = 92;
+const heightMark = 1.69;
+const heightJohn = 1.95;
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn= massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
+
+if (BMIMark > BMIJohn) {
+  console.log(`Mark's BMI is higher than John's!`);
+} else {
+   console.log(`John's BMI is higher than Mark's!`);
+}
+
+if (BMIMark > BMIJohn) {
+  console.log(`Mark's BMI (${BMIMark}) is higher than John's BMI (${BMIJohn})`);
+} else {
+   console.log(`John's BMI (${BMIJohn}) is higher than Mark's BMI (${BMIMark})`);
+}
