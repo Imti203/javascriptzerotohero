6cacebaecf4f6caeb5bf9b84ff8795cc02e8e990
unknown <imtiazshakil203@gmail.com>
2024-01-31 18:37:00 +0600

    Equality Operators == vs ===

--- 1-Fundamentals-part-1/script.js
+++ 1-Fundamentals-part-1/script.js
@@ -1,24 +1,26 @@
 
 
-           // Type Conversion and Coercion //
+                // Equality Operators == vs ===
 
-// Type Conversion 
-const inputYear = '1991';
-console.log(Number(inputYear), inputYear);// String to Number convert
-console.log(Number(inputYear) + 18);
+const age = '18';
+if (age === 18) console.log('You just became an adult :D (strict)'); // '18' not equal 18
 
-console.log(Number('Jishan'));
-console.log(typeof NaN);
+if (age == 18) console.log('You just became an adult :D (loose)');// '18' = 18
 
-console.log(String(23), 23); // Number to String convert
+const luckyNumber = prompt("What's your favourite number");
+console.log(luckyNumber);
+console.log(typeof luckyNumber);
 
-//  Type Coercion
-console.log('I am ' + 23 + ' years old'); // '+' operator changes from Number to String
-console.log('23' - '10' - 3);           // '-' operator changes from String to Number
-console.log('23' / '2');              // '/' operator changes from String to Number
-console.log('23' > '18');           // '>' operator changes from String to Number
-console.log('23' * '2');          // '*' operator changes from String to Number 
-
-let n = '1' + 1; // '11'
-n = n - 1;   // 11 - 1 Beacause of '-' operator changes from String to Number
-console.log(n);
+const favourite = Number(prompt("What's your favourite number"));
+console.log(favourite);
+console.log(typeof favourite);
+if (favourite === 23) {
+  console.log(`Cool! 23 is an amazing number`);
+}else if (favourite === 9) {
+  console.log(`9 is also a cool number`);
+}else if (favourite === 7) {
+  console.log(`7 is also a cool number`);
+}else {
+  console.log(`Number is not 23 or 7 or 9`);
+}
+if (favourite !== 23) console.log('why not 23?');
