2349fd0445ba3a5349e4ded57ce4ded0fb4e4781
unknown <imtiazshakil203@gmail.com>
2024-02-07 17:36:11 +0600

    Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,3 +1,5 @@
+/*
+// Activating Strict Mode
 'use strict';
 
 let hasDriversLicense = true;
@@ -5,4 +7,27 @@ const passTest = true ;
 
 if (passTest) hasDriversLicense = true;
 if (hasDriversLicense) console.log('I can drive :D');
+*/
 
+// Functions
+
+function logger() {
+    console.log('My name is Emtiaz');
+}
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `juice with ${apples} apples ${oranges} oranges.`
+    return juice;
+}
+fruitProcessor(5, 0);
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
