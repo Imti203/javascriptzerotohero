2d4a34737127067f81287cd5830129be79e30d0b
unknown <imtiazshakil203@gmail.com>
2024-02-07 16:49:06 +0600

    Activating Strict Mode

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,1 +1,8 @@
-//New Changes
+'use strict';
+
+let hasDriversLicense = true;
+const passTest = true ;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive :D');
+
