720f212b3c9deb6df4760908681dd4b125cff3d8
unknown <imtiazshakil203@gmail.com>
2024-03-04 18:58:02 +0600

    Introduction to Arrays

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -119,7 +119,6 @@
 Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline, which works differently.
 Each team competes 3 times, and then the average of the 3 scores is calculated (so one average score per team).
 A team ONLY wins if it has at least DOUBLE the average score of the other team. Otherwise, no team wins!
-
 1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
 2. Use the function to calculate the average for both teams
 3. Create a function 'checkWinner' that takes the average score of each team as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner to the console, together with the victory points, according to the rule above. Example: "Koalas win (30 vs. 13)".
@@ -136,23 +135,65 @@ GOOD LUCK 😀
 */
 
 
-const calcAverage = (a, b, c) => (a+ b+ c) / 3;
-console.log(calcAverage(3, 4, 5));
+// const calcAverage = (a, b, c) => (a+ b+ c) / 3;
+// console.log(calcAverage(3, 4, 5));
 
 // Test 1
-const scoreDolphins = calcAverage(85, 54, 41);
-const scoreKoalas = calcAverage(23, 34, 27);
-console.log(scoreDolphins, scoreKoalas);
+// const scoreDolphins = calcAverage(85, 54, 41);
+// const scoreKoalas = calcAverage(23, 34, 27);
+// console.log(scoreDolphins, scoreKoalas);
 
-const checkWinner = function (avgDolhins, avgKoalas){
-    if(avgDolhins >= 2 * avgKoalas) {
-        console.log(`Dolphins win 🍟 (${avgDolhins} vs ${avgKoalas})`);
-    }else if (avgKoalas >= 2 * avgDolhins) {
-        console.log(`Koalas win 🍟 (${avgKoalas} vs ${avgDolhins})`);
-    }else{
-        console.log(`No team wins...`);
-    }
+// const checkWinner = function (avgDolhins, avgKoalas){
+//     if(avgDolhins >= 2 * avgKoalas) {
+//         console.log(`Dolphins win 🍟 (${avgDolhins} vs ${avgKoalas})`);
+//     }else if (avgKoalas >= 2 * avgDolhins) {
+//         console.log(`Koalas win 🍟 (${avgKoalas} vs ${avgDolhins})`);
+//     }else{
+//         console.log(`No team wins...`);
+//     }
+// }
+// checkWinner(scoreDolphins, scoreKoalas);
+
+
+///////////////////////////////////////////////////////////////////////
+// Introduction to Arrays
+///////////////////////////////////////////////////////////////////////
+
+const friend1 = 'Micheal';
+const friend2 = 'Steven';
+const friend3 = 'Peter';
+
+const friends =['Micheal', 'Steven', 'Peter'];
+console.log(friends);
+
+const y = new Array(19991, 1984, 2008, 2020);
+
+console.log(friends[2]);
+console.log(friends[0]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends [2] ='jay';
+console.log(friends);
+
+const namePerson = 'Jonas';
+const jonas = [namePerson, 'mike', 2037 - 1991, 'teacher', friends];
+console.log(jonas);
+console.log(jonas.length);
+
+// Exercise
+const calcAge = function(birthYear) {
+    return 2038 - birthYear;
 }
-checkWinner(scoreDolphins, scoreKoalas);
 
+const years = [1990, 1967, 2002, 2010, 2018];
 
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+
+console.log(age1, age2, age3);
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
+console.log(ages);
